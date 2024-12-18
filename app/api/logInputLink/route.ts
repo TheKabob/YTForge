86ddfeeff3d7c6628467708b9dc47e531530dee3
zai <zairysbigtae@gmail.com>
2024-12-18 11:31:10 +0800

    works, but in prototype testing

--- app/api/logInputLink/route.ts
+++ app/api/logInputLink/route.ts
@@ -1,9 +1,50 @@
 import { NextResponse } from "next/server";
+import prisma from "@/client";
+import axios from 'axios';
+
+const getYoutubeTitle = async (url: string): Promise<string | null> => {
+  const videoId = new URL(url).searchParams.get('v');
+  const apiKey = "AIzaSyBmxIE32sHKSBHuNTcy88gQCcecUnkflqM";
+  const apiUrl = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet`;
+
+  try {
+    const response = await axios.get(apiUrl);
+    const title = response.data.items[0].snippet.title; 
+    return title;
+  } catch (error) {
+    console.error("Error fetching the Youtube title: ", error);
+    return null;
+  }
+};
 
 export async function POST(req: Request) {
   try {
     const { link } = await req.json();
-    console.log(link);
+    const youtubeTitle = await getYoutubeTitle(link);
+    console.log(youtubeTitle, link);
+
+    await prisma.history.create({
+      data: {
+        link: link,
+        video_name: youtubeTitle,
+      }
+    });
+
+    const response = await fetch(
+      `/api/download?url=${link}`,
+      {
+        method: 'GET',
+        // 'Content-Disposition': 'attachment; filename="video.mp4"',
+      });
+
+    
+    const blob = await response.blob();
+
+    const downloadLink = document.createElement('a');
+            downloadLink.href = URL.createObjectURL(blob);
+            downloadLink.download = 'video.mp4'; // Suggest a filename
+            downloadLink.click();
+
     return NextResponse.json({ message: "Link logged succesfully" });
   } catch (error) {
     console.error(error);
--- components/inputLink.tsx
+++ components/inputLink.tsx
@@ -4,10 +4,19 @@ import { useState } from "react";
 
 export default function InputLink() {
   const [link, setLink] = useState("");
+  // const [videoUrl, setVideoUrl] = useState("");
 
-  return (
-    <div>
-      <form onSubmit={(e) => {
+  // const handleDownload = async (e) => {
+  //   e.preventDefault();
+  //
+  //   if(!videoUrl) {
+  //     alert("please enter a video url")
+  //     return;
+  //   }
+  //   setVideoUrl("a");
+  // }
+
+  async function handleOnSubmit(e: React.FormEvent) {
         e.preventDefault();
         
         try {
@@ -24,12 +33,23 @@ export default function InputLink() {
         } catch (error) {
           console.log("Error sending link to server", error);
         }
-      }}>
+  }
+
+  return (
+    <div className="flex justify-center">
+      <form onSubmit={handleOnSubmit} className="flex flex-col items-center gap-2 w-full max-w-md px-4">
         <input id="link" name="link" value={link} onChange={(e) => {
           setLink(e.target.value);
-        }} placeholder="Enter your link"/>
-        <button type="submit">Search</button>
+        }} placeholder="Enter your link"
+        className="outline-none rounded-md text-slate-900 font-semibold w-full"/>
+        <button type="submit"
+          className="bg-slate-400 rounded-md px-2 font-bold text-slate-900">Search</button>
       </form>
+      {/* <form onSubmit={handleDownload}>
+        <button type="submit"
+          className="bg-slate-400 rounded-md px-2 font-bold text-slate-900"
+        > Download Video</button>
+      </form> */}
     </div>
   )
 }
