92bace0a33a9419fac1db7c4e9fef949e62de5d1
zai <zairysbigtae@gmail.com>
2024-12-22 15:27:51 +0800

    100% functioning, except for youtube shorts..

--- app/api/logInputLink/route.ts
+++ app/api/logInputLink/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import prisma from "@/client";
 import { getYoutubeTitle } from "./getYoutubeTitle";
 
-export async function POST(req: Request) {
+export default async function POST(req: Request) {
   try {
     const { link } = await req.json();
     const youtubeTitle = await getYoutubeTitle(link);
